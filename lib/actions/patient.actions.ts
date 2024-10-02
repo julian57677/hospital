1f93f6b84ac54993c7e221a9845369f084819168
MRJULTZ\atsej <atsejulian@gmail.com>
2024-10-02 14:15:58 +0700

    redirect to register page fix

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,23 +1,21 @@
+"use server";
 import { ID, Query } from "node-appwrite"
 import { users } from "../appwrite.config"
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
-    try {
-      const newUser = await users.create(
-      ID.unique(), 
-      user.email, 
-      user.phone, 
-      undefined, 
-      user.name
-      )
+  try {
+    console.log("create user")
+    const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name)
+    return parseStringify(newUser);
+  } catch (error: any) {
+  console.log("error :", error)
+    if (error && error?.code == 409){
+      const documents =  await users.list([
+          Query.equal('email', [user.email])
+      ])
 
-    } catch (error: any) {
-      if (error && error?.code == 409){
-        const documents =  await users.list([
-            Query.equal('email', [user.email])
-        ])
-
-        return documents?.users[0]
-      }
+      return documents?.users[0]
     }
+  }
 }
